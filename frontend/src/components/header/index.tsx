456c4f03643698b6aef14e51278763080d6cf9cc
Aniruddha Gawali <73152642+AniruddhaGawali@users.noreply.github.com>
2024-12-16 22:13:56 +0530

    fix: correct spelling of "Jounery" to "Journey" in header component

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -6,7 +6,7 @@ function Header({}: Props) {
   return (
     <div className="container relative mt-10">
       <h1 className="text-center font-dm-serif-display text-5xl font-bold md:text-7xl">
-        Jounery Wise
+        Journey Wise
       </h1>
       <h3 className="mt-2 text-center text-sm font-medium md:text-xl">
         A simple and easy to use application to keep track of your travel plans
